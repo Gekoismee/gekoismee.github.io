8506bd9525dcab5afbacea0ea5932f09e2634859
Gekoismee <57382039+Gekoismee@users.noreply.github.com>
2021-01-04 23:33:14 -0500

    Update intro.js

--- js/intro.js
+++ js/intro.js
@@ -1,18 +1,20 @@
 let header = document.querySelector('#intro');
+let letter1 = document.querySelector('#letter1');
+
 let anim = [
     { t: " ", ms: 200 },
     { t: "_", ms: 200 },
     { t: " ", ms: 200 },
     { t: "_", ms: 200 },
-    { t: "G_", ms: 100 },
-    { t: "Ge_", ms: 100 },
-    { t: "Gek_", ms: 100 },
-    { t: "Geko_", ms: 100 },
-    { t: "Gekoi_", ms: 100 },
-    { t: "Gekois_", ms: 100 },
-    { t: "Gekoism_", ms: 100 },
-    { t: "Gekoisme_", ms: 100 },
-    { t: "Gekoismee_", ms: 100 },
+    { t: "G_", ms: 200 },
+    { t: "Ge_", ms: 200 },
+    { t: "Gek_", ms: 200 },
+    { t: "Geko_", ms: 200 },
+    { t: "Gekoi_", ms: 200 },
+    { t: "Gekois_", ms: 200 },
+    { t: "Gekoism_", ms: 200 },
+    { t: "Gekoisme_", ms: 200 },
+    { t: "Gekoismee_", ms: 200 },
     { t: "Gekoismee", ms: 200 },
     { t: "Gekoismee_", ms: 200 },
     { t: "Gekoismee", ms: 200 },
@@ -20,13 +22,21 @@ let anim = [
     { t: "Gekoismee", ms: 200 },
     { t: "Gekoismee", ms: 200 },
 ];
+let let1 = [
+    { t: " ", ms: 800},
+    { t: "e", ms: 200},
+    { t: "", ms: 2800},
+];
+
 let stepDenominator = 1;
 if (window.localStorage.stepDenominator)
     stepDenominator = window.localStorage.stepDenominator;
 let i = 0;
 let update = () => {
     let step = anim[i];
+    let lett1 = let1[i];
     header.innerText = step.t;
+    letter1.innerText = lett1.t;
     i++;
 
     if (i < anim.length)
