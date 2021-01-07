cc35b26c9453aca57791b42aaf78c56d7c815206
Gekoismee <57382039+Gekoismee@users.noreply.github.com>
2021-01-07 11:23:39 -0500

    Update intro_animation.js

--- js/intro_animation.js
+++ js/intro_animation.js
@@ -7,6 +7,12 @@ let anim =[
   {c:"black", ms: 100, l:"0%", t:"0%", b:"0%", r:"0%"},
   {c:"lightseagreen", ms: 100, l:"2%", t:"2%", b:"2%", r:"2%"},
   {c:"lightseagreen", ms: 100, l:"4%", t:"4%", b:"4%", r:"4%"},
+  {c:"lightseagreen", ms: 100, l:"6%", t:"6%", b:"6%", r:"6%"},
+  {c:"lightseagreen", ms: 100, l:"8%", t:"8%", b:"8%", r:"8%"},
+  {c:"lightseagreen", ms: 100, l:"10%", t:"10%", b:"10%", r:"10%"},
+  {c:"lightseagreen", ms: 100, l:"12%", t:"12%", b:"12%", r:"12%"},
+  {c:"lightseagreen", ms: 100, l:"14%", t:"14%", b:"14%", r:"14%"},
+  {c:"lightseagreen", ms: 100, l:"16%", t:"16%", b:"16%", r:"16%"},
 ]
 
 let stepDenominator = 1;
@@ -28,7 +34,10 @@ let update = () => {
     if (i < anim.length){
         setTimeout(update, step.ms / stepDenominator);
     }else {
-        header.classList.add('top');
+        let1.classList.add('top');
+        let2.classList.add('top');
+        let3.classList.add('top');
+        let4.classList.add('top');
         setTimeout(() => {
             document.getElementById('main').style.opacity = 1;
         }, 500);
