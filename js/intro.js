991f4f93d9f6825bd339e90a75d27a917e3e9c1e
Gekoismee <57382039+Gekoismee@users.noreply.github.com>
2021-01-06 20:42:51 -0500

    Update intro.js

--- js/intro.js
+++ js/intro.js
@@ -1,21 +1,21 @@
 let header = document.querySelector('#intro');
 
 let anim = [
-    { t: " ", ms: 200 },
-    { t: "_", ms: 200 },
-    { t: " ", ms: 200 },
-    { t: "_", ms: 200 },
-    { t: "G_", ms: 200 },
-    { t: "Ge_", ms: 200 },
-    { t: "Gek_", ms: 200 },
-    { t: "Geko_", ms: 200 },
-    { t: "Gekoi_", ms: 200 },
-    { t: "Gekois_", ms: 200 },
-    { t: "Gekoism_", ms: 200 },
-    { t: "Gekoisme_", ms: 200 },
-    { t: "Gekoismee_", ms: 200 },
-    { t: "Gekoismee", ms: 200 },
-    { t: "Gekoismee", ms: 200 },
+    { t: " ", ms: 200, l: "0%" },
+    { t: "_", ms: 200, l: "10%" },
+    { t: " ", ms: 200, l: "20%" },
+    { t: "_", ms: 200, l: "30%" },
+    { t: "G_", ms: 200, l: "40%" },
+    { t: "Ge_", ms: 200, l: "50%" },
+    { t: "Gek_", ms: 200, l: "60%" },
+    { t: "Geko_", ms: 200, l: "70%" },
+    { t: "Gekoi_", ms: 200, l: "80%" },
+    { t: "Gekois_", ms: 200, l: "90%" },
+    { t: "Gekoism_", ms: 200, l: "100%" },
+    { t: "Gekoisme_", ms: 200, l: "100%" },
+    { t: "Gekoismee_", ms: 200, l: "100%" },
+    { t: "Gekoismee", ms: 200, l: "100%" },
+    { t: "Gekoismee", ms: 200, l: "100%" },
 ];
 
 
@@ -26,6 +26,7 @@ let i = 0;
 let update = () => {
     let step = anim[i];
     header.innerText = step.t;
+    header.style.left = step.l;
     i++;
 
     if (i < anim.length){
