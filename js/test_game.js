dfdabfec50cec4f1585a5ee110afb18d3834203e
Gekoismee <dablake2003@gmail.com>
2021-03-12 23:51:03 -0500

    Create test_game.js

---
+++ js/test_game.js
@@ -0,0 +1,6 @@
+let inputBtn = document.getElementById("txt");
+let outputTxt =  document.getElementById("output");
+inputBtn.onclick = convert;
+function convert(){
+   outputTxt.innerHTML = "test";
+}
