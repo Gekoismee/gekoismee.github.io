47bf1e54fa466693ef31e5ef62900859b69d181a
Gekoismee <dablake2003@gmail.com>
2021-07-14 15:47:29 -0400

    Create configuration.js

---
+++ Flashcards/js/configuration.js
@@ -0,0 +1,41 @@
+let optionsButtons = document.getElementById('optionsButtons');
+let applyBtn = document.getElementById('applyBtn');
+
+let temp;
+let choices=[];
+
+function pick(self){
+    temp = toHTML(self);
+    if(temp.classList.contains('unpicked')){
+        temp.classList.remove('unpicked');
+        temp.classList.add('picked');
+    }else if(temp.classList.contains('picked')){
+        temp.classList.remove('picked');
+        temp.classList.add('unpicked');
+    }
+}
+
+function toHTML(name){
+    return document.getElementById(name);
+}
+
+function setChoices(){
+    for(let i = 0; i < 25;i++){
+        choices.pop();
+    }
+    let choice = 'choice';
+    for(let i = 1; i <25;i++){
+        temp = choice.concat(i);
+        let temp2 = toHTML(temp);
+        if(temp2.classList.contains('picked')){
+            choices.push(i);
+        }
+    }
+    window.localStorage.choices = choices;
+    applyBtn.innerHTML = 'Applied!';
+    
+}
+
+function clearLocal(){
+    window.localStorage.choices = null;
+}
